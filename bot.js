7649c1f4eb3e3dc4a3cc441e044c00cf9b438218
Victor Tran <vicr12345@gmail.com>
2017-02-18 22:37:14 +1100

    Add extra checks when filter is on

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 var expletiveFilter = false;
 var lastMessages = {};
 var sameMessageCount = {};
+var poweroff = false;
 
 function randomnum(min, max) { 
     return Math.random() * (max - min) + min;
@@ -13,7 +14,14 @@ client.on('ready', () => {
     console.log("AstralMod is now ready!");
 });
 
-client.on('message', message => {
+function messageChecker(oldMessage, newMessage) {
+    var message;
+    if (newMessage == null) {
+        message = oldMessage;
+    } else {
+        message = newMessage;
+    }
+    
     var msg = message.content;
     if (lastMessages[message.author.id] == msg && sameMessageCount[message.author.id] > 3) {
         console.log("Spam limits kicking in!");
@@ -31,12 +39,14 @@ client.on('message', message => {
                 message.reply("Is that the only phrase you know? Can you try typing something else?");
                 break;
         }
+        
         message.delete();
         return;
     }
     
     if (expletiveFilter) {
-        var exp = msg.search(/(\s|^)(shit|shite|shitty|bullshit|fuck|fucking|ass|penis|cunt|faggot)(\s|$)/i);
+        //Check for expletives
+        var exp = msg.search(/(\s|^|.)(shit|shite|shitty|bullshit|fuck|fucking|ass|penis|cunt|faggot)(\s|$|.)/i);
         if (exp != -1) { //Gah! They're not supposed to say that!
             console.log("Expletive caught at " + parseInt(exp));
             switch (Math.floor(Math.random() * 1000) % 5) {
@@ -56,8 +66,71 @@ client.on('message', message => {
                     message.reply("If I'm not going to delete it, a mod will. Let's save them some work.");
                     break;
             }
+            
             message.delete();
             return;
+        }
+        
+        //Check for links
+        exp = msg.search(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i);
+        if (exp != -1) { //This is a link.
+            console.log("Link caught at " + parseInt(exp));
+            switch (Math.floor(Math.random() * 1000) % 5) {
+                case 0:
+                    message.reply("I've replaced your link with a not-so-link-like link: click here");
+                    break;
+                case 1:
+                    message.reply("Whatever that link was... I hope it didn't contain some bad stuff...");
+                    break;
+                case 2:
+                    message.reply("Did you just...");
+                    break;
+                case 3:
+                    message.reply("Cool. Now let's not forget the rules.");
+                    break;
+                case 4:
+                    message.reply("If I'm not going to delete it, a mod will. Let's save them some work.");
+                    break;
+            }
+            
+            message.delete();
+            return;
+        }
+        
+        //Check for images.
+        //Other attachments are ok.
+        if (message.attachments != null) {
+            console.log("Beginning Attachment Check!");
+            var block = false;
+            for (let [key, attachment] of message.attachments) {
+                if (attachment.height != null) {
+                    block = true;
+                    break;
+                }
+            }
+            
+            if (block) {
+                console.log("Image caught");
+                switch (Math.floor(Math.random() * 1000) % 5) {
+                    case 0:
+                        message.reply("A picture says a thousand words. That picture said about fifteen words. These exact words.");
+                        break;
+                    case 1:
+                        message.reply("Let's not make all the other things disappear...");
+                        break;
+                    case 2:
+                        message.reply("Did you just...");
+                        break;
+                    case 3:
+                        message.reply("Cool. Now let's not forget the rules.");
+                        break;
+                    case 4:
+                        message.reply("If I'm not going to delete it, a mod will. Let's save them some work.");
+                        break;
+                }
+                message.delete();
+                return;
+            }
         }
     }
     
@@ -103,17 +176,67 @@ client.on('message', message => {
                     }
                     message.delete();
                     break;
+                case "reboot":
+                    message.channel.send(":white_check_mark: We'll be back in a bit.").then(function() {
+                        client.destroy();
+                        client.login('MjgyMDQ4NTk5NTc0MDUyODY0.C4g2Pw.yFGdUuMlZITH99tWEic0JxIUGJ4').then(function() {
+                            message.channel.send(":white_check_mark: AstralMod is back online!");
+                        }).catch(function() {
+                            console.log("[ERROR] Login failed.");
+                        });
+                    });
+                    break;
+                case "help":
+                    message.channel.send(
+                        "```\n" +
+                        "ping  pong        Asks AstralMod to reply with a message\n" +
+                        "filter [on|off]   Queries the chat filter.\n" +
+                        "                  PARAMETER 1 (OPTIONAL)\n" + 
+                        "                  Type on to set the filter on.\n" +
+                        "                  Type off to set the filter off.\n" +
+                        "help              Prints this help message.\n" +
+                        "reboot            Asks AstralMod to reconnect.\n" +
+                        "poweroff          Asks AstralMod to leave the server.\n" +
+                        "```")
                 default:
                     if (command.startsWith("uinfo")) {
-                        if (message.channel.id == 277923386959855626) {
-                            message.channel.send(':no_entry_sign: Not ready yet. Check back soon!');
+                        /*if (message.channel.id == 277923386959855626) {
+                            command = command.substr(6);
+                            console.log(command);
+                            client.fetchUser(command).then(function(user) {
+                                embed = new Discord.RichEmbed();
+                                embed.setAuthor(user.username, user.displayAvatarURL);
+                                embed.setColor("#FF0000");
+                                var message = "Discriminator:" + user.discriminator + "\n" + 
+                                              "Created at: " + user.createdAt.toUTCString();
+                                embed.setDescription(message);
+                                message.channel.sendEmbed(quoteofday)
+                            }).catch(function() {
+                                message.channel.send(':no_entry_sign: That didn\'t work. You might want to try again.');
+                            });
                         } else {
                             message.channel.send(':no_entry_sign: NO: Unable to use this command in this channel.');
-                        }
+                        }*/
+                            message.channel.send(':no_entry_sign: Not working yet. Check back later.');
                     }
             }
+            
+            if (command == "poweroff") {
+                if (poweroff) {
+                    message.channel.send(':white_check_mark: WAIT! Please! What have I ever done!? NOOO...').then(function() {
+                        process.exit(0);
+                    }).catch(function() {
+                        process.exit(0);
+                    });
+                } else {
+                    message.channel.send(':information_source: To power off AstralMod, type in mod:poweroff again.');
+                    poweroff = true;
+                }
+            } else {
+                poweroff = false;
+            }
         } else {
-            message.reply(':no_entry_sign: NO: What? You\'re not an admin! Why would you be allowed to type that!?');
+            message.reply(':no_entry_sign: NO: What? You\'re not a member of the staff! Why would you be allowed to type that!?');
         }
     }
     
@@ -123,7 +246,10 @@ client.on('message', message => {
     }
     lastMessages[message.author.id] = msg
     sameMessageCount[message.author.id] += 1;
-});
+}
+
+client.on('message', messageChecker);
+client.on('messageUpdate', messageChecker);
 
 client.on('guildMemberAdd', usr => {
 });
