3c590b19b82ec9f97eab10c041ac82a8d9722979
Victor Tran <vicr12345@gmail.com>
2017-04-20 18:07:42 +1000

    Prevent attacking everyone

--- bot.js
+++ bot.js
@@ -667,7 +667,11 @@ function messageChecker(oldMessage, newMessage) {
                         commandProcessed = true;
                     } else if (command.startsWith("attack")) {
                         command = command.substr(7);
-                        message.channel.send("<@" + message.author.id + "> :right_facing_fist: " + command);
+                        if (command.indexOf("@everyone") == -1) {
+                            message.channel.send("<@" + message.author.id + "> :right_facing_fist: " + command);
+                        } else {
+                            message.reply("Nice try, but I ain't going to interrupt everyone. Kinda nice to not be bothered.");
+                        }
                         commandProcessed = true;
                     }
             }
