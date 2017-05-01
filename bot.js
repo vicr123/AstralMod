20179947cf17559d5d159e18a2f4b06d4613aba2
MrJed385 <mrjed385@gmail.com>
2017-05-01 11:25:25 +0100

    Added ALA suggestion channel

--- bot.js
+++ bot.js
@@ -669,7 +669,7 @@ function messageChecker(oldMessage, newMessage) {
                     commandProcessed = true;
                     break;
                 case "suggest":
-                    if (message.guild.id == 277922530973581312 || message.guild.id == 234414439330349056) {
+                    if (message.guild.id == 277922530973581312 || message.guild.id == 234414439330349056 || message.guild.id == 297057036292849680) {
                         suggestStates[message.author.id] = {};
                         suggestStates[message.author.id].state = 1;
                         suggestStates[message.author.id].guild = message.guild.id;
