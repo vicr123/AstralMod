91c29ce109df2112cd19977f0130d72fdfea5749
Victor Tran <vicr12345@gmail.com>
2018-01-06 13:22:19 +1100

    Emergency timer fix

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@
 
 var amVersion;
 if (process.argv.indexOf("--blueprint") == -1) {
-    amVersion = "2.7.0";
+    amVersion = "2.7.1";
     global.prefix = "am:";
 } else {
     amVersion = "Blueprint";
