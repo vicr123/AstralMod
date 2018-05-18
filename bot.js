5d07dbe01721484e5c59835031657f8ef76d20be
Victor Tran <vicr12345@gmail.com>
2018-05-18 13:13:17 +1000

    Increment version number

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@
 
 var amVersion;
 if (process.argv.indexOf("--blueprint") == -1) {
-    amVersion = "2.9";
+    amVersion = "2.9.1";
     global.prefix = "am:";
 } else {
     amVersion = "Blueprint";
