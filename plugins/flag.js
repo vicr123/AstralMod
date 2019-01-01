2fb3f1bfb0f1abc4c93df0f388e6c18d56de597b
John Tur <reflectronic@outlook.com>
2019-01-01 05:49:51 -0500

    Updated copyright

--- plugins/flag.js
+++ plugins/flag.js
@@ -1,7 +1,7 @@
 /****************************************
  *
  *   Flags: Plugin for AstralMod that contains flagging commands
- *   Copyright (C) 2018 Victor Tran, John Tur
+ *   Copyright (C) 2019 Victor Tran, John Tur
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
--- plugins/monitor.js
+++ plugins/monitor.js
@@ -1,7 +1,7 @@
 /****************************************
  *
  *   Monitor: Plugin for AstralMod that gives monitoring information about AstralMod
- *   Copyright (C) 2017 Victor Tran
+ *   Copyright (C) 2019 Victor Tran, John Tur
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
